1aba731e3bd8f453cbfcd97ae8f0d851864b20ef
Yonatan Rab <yonatan.rab@gmail.com>
2022-11-16 19:26:13 +0200

    fix: add nofilp mark

--- src/components/HueSlider/HueSlider.styled.ts
+++ src/components/HueSlider/HueSlider.styled.ts
@@ -9,7 +9,7 @@ const Styled = {
       '& .MuiSlider-rail': {
         opacity: 1,
         background:
-          'linear-gradient(to right, rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%)'
+          'linear-gradient(to right, rgb(255, 0, 0) 0%, rgb(255, 255, 0) 17%, rgb(0, 255, 0) 33%, rgb(0, 255, 255) 50%, rgb(0, 0, 255) 67%, rgb(255, 0, 255) 83%, rgb(255, 0, 0) 100%) /*! @noflip */'
       },
 
       '& .MuiSlider-track': {
--- src/shared/constants/styles.ts
+++ src/shared/constants/styles.ts
@@ -1,5 +1,5 @@
 export const BG_IMAGE_FALLBACK =
-  'linear-gradient(45deg, #ccc 25%, transparent 25%), linear-gradient(135deg, #ccc 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #ccc 75%), linear-gradient(135deg, transparent 75%, #ccc 75%)'
+  'linear-gradient(45deg, #ccc 25%, transparent 25%), linear-gradient(135deg, #ccc 25%, transparent 25%), linear-gradient(45deg, transparent 75%, #ccc 75%), linear-gradient(135deg, transparent 75%, #ccc 75%) /*! @noflip */'
 
 export const BG_IMAGE_SPACE =
-  'linear-gradient(to top, #000000, transparent), linear-gradient(to right, #ffffff, transparent)'
+  'linear-gradient(to top, #000000, transparent), linear-gradient(to right, #ffffff, transparent) /*! @noflip */'
