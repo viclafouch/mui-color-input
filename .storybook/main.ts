8efd78a7564557b33b03c2e93f1351a4be273a8b
viclafouch <victor.dlf@outlook.fr>
2024-08-29 11:09:37 +0200

    chore: update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,8 @@ const config: StorybookConfig = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@chromatic-com/storybook'
   ],
 
   framework: {
